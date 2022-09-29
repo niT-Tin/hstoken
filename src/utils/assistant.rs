7a5ea7020ac64e2f4d2262650ab564f02e778b68
hachss <liuzehao523@gmail.com>
2022-09-30 06:33:48 +0800

    feature:添加范围检测函数

--- src/utils/assistant.rs
+++ src/utils/assistant.rs
@@ -19,8 +19,27 @@ lazy_static! {
      */
     static ref REOPERATOR: Regex = Regex::new(r"^[+\-\\*/|!<>&^,;]$").unwrap();
     // static ref REOPERATOR: Regex = Regex::new(r"(\+|-|\*|/|\||!|<|>|&|\^|,|;)").unwrap();
+    /*
+     * 匹配[0-9]的数字
+     */
+    static ref RANGE09: Regex = Regex::new(r"[0-9]$").unwrap();
+    /*
+     * 匹配[1-9]的数字
+     */
+    static ref RANGE19: Regex = Regex::new(r"[1-9]$").unwrap();
+    /*
+     * 匹配[0-7]的数字
+     */
+    static ref RANGE07: Regex = Regex::new(r"[0-7]$").unwrap();
+    /*
+     * 匹配[1-7]的数字
+     */
+    static ref RANGE17: Regex = Regex::new(r"[1-7]$").unwrap();
+    /*
+     * 匹配十六进制数字面值
+     */
+    static ref RANGE09AF: Regex = Regex::new(r"[0-9A-F]$").unwrap();
 }
-
 
 pub struct Assistant;
 
@@ -43,5 +62,25 @@ impl Assistant {
     // 判读字符是否为操作符
     pub fn is_operator(&self, src: &char) -> bool {
         REOPERATOR.is_match(&src.to_string())
+    }
+
+    pub fn in09(&self, src: &char) -> bool {
+        RANGE09.is_match(&src.to_string())
+    }
+
+    pub fn in19(&self, src: &char) -> bool {
+        RANGE19.is_match(&src.to_string())
+    }
+
+    pub fn in07(&self, src: &char) -> bool {
+        RANGE07.is_match(&src.to_string())
+    }
+
+    pub fn in17(&self, src: &char) -> bool {
+        RANGE17.is_match(&src.to_string())
+    }
+
+    pub fn in09af(&self, src: &char) -> bool {
+        RANGE09AF.is_match(&src.to_string())
     }
 }
