29fdddac35bb57c56e1adfa57cd8d3dd40495333
hachss <liuzehao523@gmail.com>
2022-09-30 00:06:06 +0800

    fix:Assistant内部函数共有，修正用于匹配字符和操作符的正则表达式

--- src/utils/assistant.rs
+++ src/utils/assistant.rs
@@ -9,7 +9,7 @@ lazy_static! {
     /*
      * 字符正则表达式
      */
-    static ref RELETTER: Regex = Regex::new(r"^[_a-zA-Z]$").unwrap();
+    static ref RELETTER: Regex = Regex::new(r"^[a-zA-Z]$").unwrap();
     /*
      * 字面值正则表达式
      */
@@ -17,7 +17,8 @@ lazy_static! {
     /*
      * 运算符正则表达式
      */
-    static ref REOPERATOR: Regex = Regex::new(r"[+-\*/|!<>&^]$").unwrap();
+    static ref REOPERATOR: Regex = Regex::new(r"^[+\-\\*/|!<>&^,;]$").unwrap();
+    // static ref REOPERATOR: Regex = Regex::new(r"(\+|-|\*|/|\||!|<|>|&|\^|,|;)").unwrap();
 }
 
 
@@ -25,22 +26,22 @@ pub struct Assistant;
 
 impl Assistant {
     // 判断字符是否为数字
-    fn is_number(&self, src: char) -> bool {
+    pub fn is_number(&self, src: char) -> bool {
         RENUMBER.is_match(&src.to_string())
     }
 
     // 判断字符是否为字符
-    fn is_letter(&self, src: char) -> bool {
+    pub fn is_letter(&self, src: char) -> bool {
         RELETTER.is_match(&src.to_string())
     }
 
     // 判读字符是否为字面值
-    fn is_literal(&self, src: char) -> bool {
+    pub fn is_literal(&self, src: char) -> bool {
         RELITERAL.is_match(&src.to_string())
     }
 
     // 判读字符是否为操作符
-    fn is_operator(&self, src: char) -> bool {
+    pub fn is_operator(&self, src: char) -> bool {
         REOPERATOR.is_match(&src.to_string())
     }
 }
