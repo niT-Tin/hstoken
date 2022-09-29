e6e29d6cf933141837f105cc4b3039078131ed2e
hachss <liuzehao523@gmail.com>
2022-09-30 06:31:33 +0800

    feature:实现构造数值字面量功能

--- src/lexical/models.rs
+++ src/lexical/models.rs
@@ -1,10 +1,12 @@
+use std::collections::HashMap;
+
 use crate::utils::assistant::Assistant;
 
-use self::Tokens::Token;
+use self::Tokens::{Token, TokenType};
 
 /// 词法分析相关model
 pub mod Tokens {
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     pub enum TokenType {
         VARIABLE, // 变量
         KEYWORD,  // 关键字
@@ -20,9 +22,10 @@ pub mod Tokens {
         INTEGER, // 整型
         HEX,     // 十六进制
         OCT,     // 八进制
+        ERROR,   // 占位错误Token
     }
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     pub struct Token {
         pub ttype: TokenType, // Token类型
         pub tvalue: String,   // Token值
@@ -38,6 +41,18 @@ pub mod Tokens {
         LITERAL,  // 字面值
         OPERATOR, // 操作符
     }
+}
+
+// 用于记录哪些状态下能够产生结果，应对最终数字后没有其他字符
+lazy_static! {
+    static ref res_map: HashMap<i32, TokenType> = HashMap::from([
+        (1, TokenType::OCT),
+        (4, TokenType::INTEGER),
+        (5, TokenType::FLOAT),
+        (6, TokenType::FLOAT),
+        (7, TokenType::OCT),
+        (9, TokenType::HEX),
+    ]);
 }
 
 // 关键字
@@ -119,12 +134,21 @@ impl Token {
             it.next();
         }
         if token == String::from("tRUE") || token == String::from("fALSE") {
-            Token{ttype: TP::BOOLEAN, tvalue: token}
+            Token {
+                ttype: TP::BOOLEAN,
+                tvalue: token,
+            }
         // 判断结果是否在关键字列表中
         } else if KEYWORDS.contains(&token) {
-            Token{ttype: TP::KEYWORD, tvalue: token}
+            Token {
+                ttype: TP::KEYWORD,
+                tvalue: token,
+            }
         } else {
-            Token{ttype: TP::VARIABLE, tvalue: token}
+            Token {
+                ttype: TP::VARIABLE,
+                tvalue: token,
+            }
         }
     }
 
@@ -145,7 +169,7 @@ impl Token {
         let mut it = src.iter().peekable();
         while let Some(&c) = it.peek() {
             match state {
-                 STATE::START => {
+                STATE::START => {
                     if (*c).eq(&'"') {
                         state = STATE::DQUOT;
                     } else if (*c).eq(&'\'') {
@@ -153,7 +177,7 @@ impl Token {
                     }
                     token.push(*c);
                     it.next();
-                },
+                }
                 STATE::SQUOT => {
                     if (*c).eq(&'\'') {
                         token.push(*c);
@@ -161,7 +185,7 @@ impl Token {
                     }
                     token.push(*c);
                     it.next();
-                },
+                }
                 STATE::DQUOT => {
                     if (*c).eq(&'"') {
                         token.push(*c);
@@ -172,20 +196,193 @@ impl Token {
                 }
             }
         }
-        Token { ttype: TP::STRING, tvalue: token }
+        Token {
+            ttype: TP::STRING,
+            tvalue: token,
+        }
     }
 
     /*
      * 从源代码构造操作符
      */
     pub fn make_operator(src: &Vec<char>) -> Token {
-        Token{ ttype: TP::BOOLEAN, tvalue: String::from("") }
+        Token {
+            ttype: TP::BOOLEAN,
+            tvalue: String::from(""),
+        }
     }
 
     /*
      * 从源代码构造数字
      */
     pub fn make_number(src: &Vec<char>) -> Token {
-        Token{ ttype: TP::BOOLEAN, tvalue: String::from("") }
+        let mut token = String::from("");
+        let mut state = 0;
+        let mut it = src.iter().peekable();
+
+        while let Some(&c) = it.peek() {
+            match state {
+                0 => {
+                    if Assistant.in19(c) {
+                        state = 4;
+                    } else if (*c).eq(&'+') || (*c).eq(&'-') {
+                        state = 2;
+                    } else if (*c).eq(&'0') {
+                        state = 1;
+                    } else if (*c).eq(&'.') {
+                        state = 3;
+                    }
+                }
+                1 => {
+                    if (*c).eq(&'0') {
+                        state = 1;
+                    } else if Assistant.in17(c) {
+                        state = 7;
+                    } else if (*c).eq(&'.') {
+                        state = 3;
+                    } else if (*c).eq(&'x') {
+                        state = 9;
+                    } else if !Assistant.in17(c) && Assistant.is_number(c) {
+                        // TODO 抛出错误取代返回的占位Token
+                        return Token {
+                            ttype: Tokens::TokenType::ERROR,
+                            tvalue: String::from("1"),
+                        };
+                    } else {
+                        return Token {
+                            ttype: TP::OCT,
+                            tvalue: token,
+                        };
+                    }
+                }
+                2 => {
+                    if Assistant.in19(c) {
+                        state = 4;
+                    } else if (*c).eq(&'0') {
+                        state = 1;
+                    } else if (*c).eq(&'.') {
+                        state = 3;
+                    } else {
+                        // TODO 抛出错误取代返回的占位Token
+                        return Token {
+                            ttype: Tokens::TokenType::ERROR,
+                            tvalue: String::from("2"),
+                        };
+                    }
+                }
+                3 => {
+                    if Assistant.in09(c) {
+                        state = 6;
+                    } else {
+                        // TODO 抛出错误取代返回的占位Token
+                        return Token {
+                            ttype: Tokens::TokenType::ERROR,
+                            tvalue: String::from("3"),
+                        };
+                    }
+                }
+                4 => {
+                    if Assistant.in09(c) {
+                        state = 4;
+                    } else if (*c).eq(&'.') {
+                        state = 5;
+                    } else {
+                        return Token {
+                            ttype: Tokens::TokenType::INTEGER,
+                            tvalue: token,
+                        };
+                    }
+                }
+                5 => {
+                    if (*c).eq(&'.') {
+                        // TODO 抛出错误取代返回的占位Token
+                        return Token {
+                            ttype: Tokens::TokenType::ERROR,
+                            tvalue: String::from("5"),
+                        };
+                    } else if Assistant.in09(c) {
+                        state = 6;
+                    } else {
+                        return Token {
+                            ttype: Tokens::TokenType::FLOAT,
+                            tvalue: token,
+                        };
+                    }
+                }
+                6 => {
+                    if (*c).eq(&'.') {
+                        // TODO 抛出错误取代返回的占位Token
+                        return Token {
+                            ttype: Tokens::TokenType::ERROR,
+                            tvalue: String::from("6"),
+                        };
+                    } else if Assistant.in09(c) {
+                        state = 6;
+                    } else {
+                        return Token {
+                            ttype: Tokens::TokenType::FLOAT,
+                            tvalue: token,
+                        };
+                    }
+                }
+                7 => {
+                    if (*c).eq(&'.') {
+                        // TODO 抛出错误取代返回的占位Token
+                        return Token {
+                            ttype: Tokens::TokenType::ERROR,
+                            tvalue: String::from("7"),
+                        };
+                    } else if Assistant.in07(c) {
+                        state = 7;
+                    } else {
+                        return Token {
+                            ttype: Tokens::TokenType::OCT,
+                            tvalue: token,
+                        };
+                    }
+                }
+                9 => {
+                    if (*c).eq(&'.') {
+                        // TODO 抛出错误取代返回的占位Token
+                        return Token {
+                            ttype: Tokens::TokenType::ERROR,
+                            tvalue: String::from("9"),
+                        };
+                    } else if Assistant.in09af(c) {
+                        state = 9;
+                    } else if !Assistant.in09af(c) && Assistant.is_literal(c) {
+                        // TODO 抛出错误取代返回的占位Token
+                        return Token {
+                            ttype: Tokens::TokenType::ERROR,
+                            tvalue: String::from("9"),
+                        };
+                    } else {
+                        return Token {
+                            ttype: Tokens::TokenType::HEX,
+                            tvalue: token,
+                        };
+                    }
+                }
+                _ => {
+                    // TODO 抛出错误取代返回的占位Token
+                    return Token {
+                        ttype: Tokens::TokenType::ERROR,
+                        tvalue: String::from("O"),
+                    };
+                }
+            }
+            token.push(*c);
+            it.next();
+        }
+        // 判断是从那个有结果的分支出来的
+        match res_map.get(&state) {
+            // 如果是有结果的分支，则返回对应结果
+            Some(s) => Token { ttype: (*s).clone(), tvalue: token },
+            // 如果没有结果则返回错误
+            None => Token {
+                ttype: TP::ERROR,
+                tvalue: String::from("outer"),
+            },
+        }
     }
 }
