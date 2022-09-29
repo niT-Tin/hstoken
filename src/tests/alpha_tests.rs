3742f4861bfa37b4042f7cae2c73c7c3858951d3
hachss <liuzehao523@gmail.com>
2022-09-30 06:32:10 +0800

    添加范围测试

--- src/tests/alpha_tests.rs
+++ src/tests/alpha_tests.rs
@@ -40,4 +40,41 @@ mod alpha_test {
         assert_eq!(true, Assistant.is_operator(&';'));
         assert_eq!(false, Assistant.is_operator(&'9'));
     }
+
+    #[test]
+    fn assiatant_range() {
+        assert_eq!(true, Assistant.in07(&'6'));
+        assert_eq!(true, Assistant.in07(&'7'));
+        assert_eq!(true, Assistant.in07(&'0'));
+        assert_eq!(false, Assistant.in07(&'8'));
+        // 字母o
+        assert_eq!(false, Assistant.in07(&'o'));
+
+
+        assert_eq!(true, Assistant.in09(&'8'));
+        assert_eq!(true, Assistant.in09(&'9'));
+        assert_eq!(true, Assistant.in09(&'0'));
+        assert_eq!(false, Assistant.in09(&'A'));
+
+
+        assert_eq!(true, Assistant.in17(&'6'));
+        assert_eq!(false, Assistant.in17(&'8'));
+        assert_eq!(true, Assistant.in17(&'1'));
+        assert_eq!(false, Assistant.in17(&'0'));
+        assert_eq!(false, Assistant.in17(&'A'));
+
+        assert_eq!(true, Assistant.in19(&'8'));
+        assert_eq!(true, Assistant.in19(&'9'));
+        assert_eq!(true, Assistant.in19(&'1'));
+        assert_eq!(false, Assistant.in19(&'0'));
+        assert_eq!(false, Assistant.in19(&'A'));
+
+
+        assert_eq!(true, Assistant.in09af(&'0'));
+        assert_eq!(true, Assistant.in09af(&'9'));
+        assert_eq!(true, Assistant.in09af(&'1'));
+        assert_eq!(true, Assistant.in09af(&'A'));
+        assert_eq!(true, Assistant.in09af(&'F'));
+        assert_eq!(false, Assistant.in09af(&'Z'));
+    }
 }
