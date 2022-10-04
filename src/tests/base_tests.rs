0812b812ad84b1ed61d76e012f2c85eb6bbb756f
hachss <liuzehao523@gmail.com>
2022-10-05 00:56:41 +0800

    基础数据结构测试

---
+++ src/tests/base_tests.rs
@@ -0,0 +1,42 @@
+#[cfg(test)]
+mod base_tests {
+    use crate::base::base::PPUTKIterator;
+
+    #[test]
+    fn base_test1() {
+        let src = "ABCDEFGHIJKLNM".chars().collect::<Vec<char>>();
+        let mut ppuk = PPUTKIterator::new(src.iter());
+
+        assert_eq!(ppuk.next().unwrap(), &'A');
+        assert_eq!(ppuk.next().unwrap(), &'B');
+        assert_eq!(ppuk.next().unwrap(), &'C');
+        assert_eq!(ppuk.next().unwrap(), &'D');
+        assert_eq!(ppuk.next().unwrap(), &'E');
+
+        ppuk.put_back();
+        ppuk.put_back();
+        ppuk.put_back();
+
+        assert_eq!(ppuk.peek().unwrap(), &'C');
+        assert_eq!(ppuk.peek().unwrap(), &'C');
+        assert_eq!(ppuk.peek().unwrap(), &'C');
+
+
+        assert_eq!(ppuk.next().unwrap(), &'C');
+        assert_eq!(ppuk.next().unwrap(), &'D');
+        assert_eq!(ppuk.next().unwrap(), &'E');
+
+        assert_eq!(ppuk.next().unwrap(), &'F');
+        assert_eq!(ppuk.peek().unwrap(), &'G');
+        ppuk.put_back();
+        assert_eq!(ppuk.peek().unwrap(), &'F');
+        assert_eq!(ppuk.next().unwrap(), &'F');
+
+        while let Some(s) = ppuk.peek() {
+            println!("{:?}", s);
+            ppuk.next();
+        }
+
+
+    }
+}
